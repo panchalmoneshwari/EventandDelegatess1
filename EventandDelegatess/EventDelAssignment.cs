d7c322dcc5a71287c2236ef5dae81f6c739bd58d
Moneshwari Panchal <moneshwaripanchal@gmail.com>
2022-06-15 13:51:30 +0530

    Add project files.

---
+++ EventandDelegatess/EventDelAssignment.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventandDelegatess
+{
+    public delegate void Mydel();
+    class Bank
+    {
+        int act_bal;
+        public event Mydel Zero;
+        public event Mydel Inefficient_bal;
+
+        public Bank(int act_bal)
+        {
+            this.act_bal = act_bal;
+        }
+        public void Debit(int debit_amt)
+        {
+            if (act_bal == 0)
+            {
+                Zero(); // call to the event or raise an event
+            }
+            else if (act_bal < debit_amt)
+            {
+                Inefficient_bal();
+            }
+            else
+            {
+                act_bal -= debit_amt;
+                Console.WriteLine($"Your Acount balance is {act_bal}");
+            }
+        }
+        public void Credit(int credit_amt)
+        {
+            act_bal += credit_amt;
+            Console.WriteLine($"Your Acount balance is {act_bal}");
+        }
+
+    }
+    class EventDelAssignment
+    {
+        static void ZeroMsg()
+        {
+            Console.WriteLine("Your Acount Balance is zero.");
+        }
+        static void Inefficient_balMsg()
+        {
+            Console.WriteLine("Your Acount has Inefficient Balance.");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter amount");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Bank b = new Bank(n);
+            b.Zero += new Mydel(ZeroMsg);
+            b.Inefficient_bal += new Mydel(Inefficient_balMsg);
+            Console.WriteLine("Enter Debit Amount");
+            int d = Convert.ToInt32(Console.ReadLine());
+            b.Debit(d);
+            Console.WriteLine("Enter credit Amount");
+            int c = Convert.ToInt32(Console.ReadLine());
+            b.Credit(c);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EventandDelegatess/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventandDelegatess
+{
+    //delegate is ref type in c#,it is type,declare inside namespace
+    public delegate void Mydel1(string a);
+    public class strmanupulation
+    {
+        public void Uppercase(string name)
+        {
+            Console.WriteLine(name.ToUpper());
+        }
+        public void Lowercase(string name)
+        {
+            Console.WriteLine(name.ToLower());
+        }
+    }
+    class Case_change
+    {
+        static void Main(string[] args)
+        {
+            strmanupulation str = new strmanupulation();
+            Mydel1 obj = new Mydel1(str.Uppercase);
+            obj += new Mydel1(str.Lowercase);//Add the method in invocation list
+            obj -= new Mydel1(str.Uppercase);//Remove the method from invocation list
+            //obj.Invoke("India");
+            Delegate[] list = obj.GetInvocationList();
+            foreach (Delegate a in list)
+            {
+                Console.WriteLine(a.Method);
+                a.DynamicInvoke("Pune");
+            }
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        
+        public class Student
+        {
+            public event Mydel Fail;
+            public event Mydel Distinction;
+            public void AcceptMarks(int marks)
+            {
+                if (marks < 40)
+                {
+                    Fail(); // call to the event or raise an event
+                }
+                else if (marks >= 75)
+                {
+                    Distinction();
+                }
+                Console.WriteLine($"Your Score is {marks}");
+            }
+        }
+        class Result
+        {
+            static void FailMsg()
+            {
+                Console.WriteLine("You are fail !");
+            }
+            static void DistinctionMsg()
+            {
+                Console.WriteLine("Congratulations ! You are pass with Distinction");
+            }
+            static void Main(string[] args)
+            {
+                Student s1 = new Student();
+                s1.Fail += new Mydel(FailMsg);
+                s1.Distinction += new Mydel(DistinctionMsg);
+                s1.AcceptMarks(30);
+                Console.ReadLine();
+            }
+
+        }
+
+
+    }
+   
+}
